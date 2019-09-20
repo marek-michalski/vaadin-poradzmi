8c4469c1ee7fe5c66ba07e82cb68ca552a263c92
Marek Michalski <nydmif@gmail.com>
2019-09-20 12:21:00 +0200

    new class Account

--- src/main/java/pl/sda/vaadinporadzmi/Layout.java
+++ src/main/java/pl/sda/vaadinporadzmi/Layout.java
@@ -1,7 +1,9 @@
 package pl.sda.vaadinporadzmi;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -29,11 +31,41 @@ public class Layout extends Div {
         content.addClassName("content");
         footer.addClassName("footer");
 
+
         // Add content
-        header.add("HEADER");
-        menu.add("MENU");
-        content.add("CONTENT");
-        footer.add("FOOTER");
+        header.add(new H3("PORADŹ MI! :)"));
+        menu.add(new H4("MENU"));
+        content.add(new H4("CONTENT"));
+        footer.add(new H6("Copyright © 2019 by Marek Michalski."));
+
+        Button buttonSearch = new Button("Szukaj");
+        buttonSearch.addClickListener(click -> {
+            Notification.show("Thanks for clicking!");
+        });
+
+        Button buttonQuestion = new Button("Zadaj pytanie");
+        buttonQuestion.addClickListener(click -> {
+            Notification.show("Thanks for clicking!");
+        });
+
+        Button buttonNewAccount = new Button("Utwórz konto");
+        buttonNewAccount.addClickListener(click -> {
+            Notification.show("Thanks for clicking!");
+        });
+
+        Button buttonLogin = new Button("Zaloguj");
+        buttonLogin.addClickListener(click -> {
+            Notification.show("Thanks for clicking!");
+        });
+
+        Label label = new Label("Tutaj powstanie jakiś wpis...");
+
+        header.add(buttonSearch);
+        menu.add(buttonQuestion);
+        menu.add(buttonNewAccount);
+        menu.add(buttonLogin);
+        content.add(label);
+
 
         // Compose layout
         center.add(menu, content);
